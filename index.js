b56142a82596f394decca1a0da39de90dfa58746
Kosi74 <kaosinwankwo74@gmail.com>
2024-02-17 23:44:35 +0100

    displayed the input from the login screen to another route

--- index.js
+++ index.js
@@ -2,24 +2,31 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
+var fName = "";
+var lName = "";
 
 app.use(bodyParser.urlencoded({ extended: true }));
+
+function nameGenerator(req, res, next) {
+    console.log(req.body);
+    fName = req.body["fName"];
+    lName = req.body["lName"]
+    next();
+}
+app.use(nameGenerator);
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
 
 app.post("/login", (req, res) => {
     console.log(req.body);
-    res.redirect("/seen");
-});
-
-app.get("/seen", (req, res) => {
-    res.sendFile(__dirname + "/public/web.html")
+res.send(`<h1>Your First Name is ${fName}</h1> \n <h2>Your Last Name is ${lName} </h2>`)
 });
 
 app.listen(port, ()=> console.log(`Server is running on port ${port}`))
